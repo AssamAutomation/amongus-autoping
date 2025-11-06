bab8f5cbbc75f6ed8a520c67ea0869fcee0ccf6a
AssamAutomation <146053919+AssamAutomation@users.noreply.github.com>
2025-11-07 01:27:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 
 WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 
-data = {"content": "Hello everyone"}
+data = {"content": "Hello everyone faltu"}
 requests.post(WEBHOOK, json=data)
 
 print("Message sent!")
