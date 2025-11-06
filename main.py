825aa93b64a3536c8b06671f271f1d7cb5a6d632
AssamAutomation <146053919+AssamAutomation@users.noreply.github.com>
2025-11-07 01:18:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
 import os
 import requests
-from flask import Flask
 
-app = Flask(__name__)
+WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 
-WEBHOOK = os.getenv("DISCORD_WEBHOOK")  # put your webhook in Render env variable
+data = {"content": "Hello everyone"}
+requests.post(WEBHOOK, json=data)
 
-@app.route("/")
-def home():
-    return "Test Webhook alive"
-
-@app.route("/test")
-def test_webhook():
-    if not WEBHOOK:
-        return "Webhook missing!"
-
-    msg = "✅ Bot is connected!\nThe lobby code will be available soon..."
-    try:
-        requests.post(WEBHOOK, json={"content": msg})
-        return "Test message sent to Discord!"
-    except Exception as e:
-        return f"Error: {e}"
-
-if __name__ == "__main__":
-    app.run()
+print("Message sent!")
